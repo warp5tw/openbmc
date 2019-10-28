32f573845c87bd05dc8c72d506560208b6877654
Medad CChien <ctcchien@nuvoton.com>
2019-10-28 09:48:59 +0800

    meta-phosphor: nuvoton-layer: recipes-phosphor: phosphor-ipmi-flash: workaround for fixing rebooting failure after updating bios
    Signed-off-by: Medad CChien <ctcchien@nuvoton.com>

--- meta-phosphor/nuvoton-layer/recipes-phosphor/ipmi/phosphor-ipmi-flash/bios-update.sh
+++ meta-phosphor/nuvoton-layer/recipes-phosphor/ipmi/phosphor-ipmi-flash/bios-update.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 i=0
-POWERDOWN_STATE="i 0"
-
-
 findmtd() {
 	m=$(grep -xl "$1" /sys/class/mtd/*/name)
 	m=${m%/name}
@@ -11,23 +8,30 @@ findmtd() {
 	echo $m
 }
 
-r=$(busctl get-property org.openbmc.control.Power /org/openbmc/control/power0 org.openbmc.control.Power state)
+r=$(cat /sys/class/gpio/gpio506/value)
 
-until [ "$i" == "100" -o "$r" == "$POWERDOWN_STATE" ]
+until [ "$i" == "10" -o "$r" == "0" ]
 do
 	i=$(($i+1))
-	echo "Shut down"
+	echo "Shut down:" $i
 	busctl set-property xyz.openbmc_project.State.Chassis \
 						/xyz/openbmc_project/state/chassis0 \
 						xyz.openbmc_project.State.Chassis \
 						RequestedPowerTransition s xyz.openbmc_project.State.Chassis.Transition.Off
-	sleep 3
-	r=$(busctl get-property org.openbmc.control.Power /org/openbmc/control/power0 org.openbmc.control.Power state)
-	echo "PowerState:"$r" Times:"$i
+	j=0
+	r=$(cat /sys/class/gpio/gpio506/value)
+	until [ "$r" == "0" -o "$j" == "60" ]
+	do
+		j=$(($j+1))
+		sleep 1
+		r=$(cat /sys/class/gpio/gpio506/value)
+		echo $j"s" " 506:" $r
+	done
 done
 
-if [ "$r" == "$POWERDOWN_STATE" ]; then
-	cho "HOST is shut down!"
+if [ "$r" == "0" ]; then
+	echo "HOST is shut down!"
+	sleep 10
 	echo "Mount BIOS flash"
 	echo 227 > /sys/class/gpio/export
 	echo 1 > /sys/class/gpio/gpio227/value
@@ -44,20 +48,37 @@ if [ "$r" == "$POWERDOWN_STATE" ]; then
 	fi
 
 	flashcp -v /tmp/bios-image /dev/$m && rm /tmp/bios-image
-	
+	sleep 10
+
 	echo "Unmount BISO flash"
 	echo 0 > /sys/class/gpio/gpio227/value
 	cd /sys/bus/platform/devices/c0000000.fiu/driver
 	echo -n "c0000000.fiu" > unbind
 	echo -n "c0000000.fiu" > bind
 
+	sleep 10
+
+	echo  0 > /sys/class/gpio/gpio505/value
+	sleep 10
+	echo  1 > /sys/class/gpio/gpio505/value
+	sleep 1
+
 	echo "Power on"
 	busctl set-property xyz.openbmc_project.State.Host \
-			/xyz/openbmc_project/state/host0 xyz.openbmc_project.State.Host \
-			RequestedHostTransition s xyz.openbmc_project.State.Host.Transition.On
-	sleep 5
-	r=$(busctl get-property org.openbmc.control.Power /org/openbmc/control/power0 org.openbmc.control.Power state)
-	echo "PowerState:"$r
+		/xyz/openbmc_project/state/host0 \
+		xyz.openbmc_project.State.Host \
+		RequestedHostTransition s xyz.openbmc_project.State.Host.Transition.On
+
+	i=0
+	r=$(cat /sys/class/gpio/gpio506/value)
+	until [ "$r" == "1" -o "$i" == "60" ]
+	do
+		i=$(($i+1))
+		sleep 1
+		r=$(cat /sys/class/gpio/gpio506/value)
+		echo $i"s" " 506:" $r
+	done
+
 else
 	echo "We could NOT power down HOST, so skip updating BIOS!!"
 fi
