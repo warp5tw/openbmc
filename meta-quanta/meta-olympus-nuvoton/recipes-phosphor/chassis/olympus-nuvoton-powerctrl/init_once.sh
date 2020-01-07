88d4008953a81416b242be1e46d0a0dc3d5379f5
Joseph Liu <kwliu@nuvoton.com>
2020-01-07 18:08:05 +0800

    meta-quanta: olympus-nuvoton: op-control-power: change power contorl pin
    From quanta suggestion, the gpio 504/505 are using for debug purpose, so
    we take gpio 451/452 as the power control pins.
    
    Signed-off-by: Joseph Liu <kwliu@nuvoton.com>

--- meta-quanta/meta-olympus-nuvoton/recipes-phosphor/chassis/olympus-nuvoton-powerctrl/init_once.sh
+++ meta-quanta/meta-olympus-nuvoton/recipes-phosphor/chassis/olympus-nuvoton-powerctrl/init_once.sh
@@ -8,7 +8,7 @@ function set_gpio_out()
 }
 
 # Set Reset ping GPIO 504 and Power pin GPIO 505 as output high with active_low
-set_gpio_out 504 high
-set_gpio_out 505 high
+set_gpio_out 451 high
+set_gpio_out 452 high
 
 exit 0;
