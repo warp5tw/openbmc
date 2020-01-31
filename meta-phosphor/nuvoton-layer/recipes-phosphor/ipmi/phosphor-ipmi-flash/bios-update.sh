3bc88b2b863a729456790f94a975b8e74e1a37aa
Brian_Ma <chma0@nuvoton.com>
2020-01-31 18:03:19 +0800

    meta-phosphor: nuvoton-layer: recipes-phosphor: phosphor-ipmi-flash: disable ME function while update bios
    Signed-off-by: Brian_Ma <chma0@nuvoton.com>

--- meta-phosphor/nuvoton-layer/recipes-phosphor/ipmi/phosphor-ipmi-flash/bios-update.sh
+++ meta-phosphor/nuvoton-layer/recipes-phosphor/ipmi/phosphor-ipmi-flash/bios-update.sh
@@ -7,9 +7,36 @@ findmtd() {
 	m=${m##*/}
 	echo $m
 }
+IPMB_LOG=/tmp/bios_flash.log
+setEnableME() {
+    echo "==== $(date) ==== $1" >> $IPMB_LOG
+    journalctl --no-p -u ipmb | tail -n 2 >> $IPMB_LOG
+    res=`busctl call xyz.openbmc_project.NodeManagerProxy \
+        /xyz/openbmc_project/status/me \
+        xyz.openbmc_project.SetHealth EnableForceRecovery b ${1}`
+    echo "==== $(date) ====" >> $IPMB_LOG
+    journalctl --no-p -u ipmb |tail -n 2 >> $IPMB_LOG
+}
+PowerOn() {
+    i=0
+    busctl set-property xyz.openbmc_project.State.Host \
+        /xyz/openbmc_project/state/host0 \
+        xyz.openbmc_project.State.Host \
+        RequestedHostTransition s \
+        xyz.openbmc_project.State.Host.Transition.On
+    r=$(cat /sys/class/gpio/gpio506/value)
+    until [ "$r" == "1" -o "$i" == "30" ]
+    do
+        i=$(($i+1))
+        sleep 1
+        r=$(cat /sys/class/gpio/gpio506/value)
+        echo "${i}s  506: ${r}, retry:${retry}"
+    done
+}
 
 r=$(cat /sys/class/gpio/gpio506/value)
 
+hostState=${r}
 until [ "$i" == "10" -o "$r" == "0" ]
 do
 	i=$(($i+1))
@@ -31,7 +58,21 @@ done
 
 if [ "$r" == "0" ]; then
 	echo "HOST is shut down!"
-	sleep 10
+    # disable ME
+    setEnableME true
+    if [ "$res" != "i 0" ];then
+        echo "disable ME fail!!"
+        # retry
+        echo "start retry..."
+        setEnableME true
+        sleep 5
+        setEnableME false
+        if [ "$res" != "i 0" ];then
+            echo "disable ME failed."
+            exit 1
+        fi
+    fi
+    sleep 1
 	echo "Mount BIOS flash"
 	echo 227 > /sys/class/gpio/export
 	echo 1 > /sys/class/gpio/gpio227/value
@@ -56,28 +97,32 @@ if [ "$r" == "0" ]; then
 	echo -n "c0000000.fiu" > unbind
 	echo -n "c0000000.fiu" > bind
 
-	sleep 10
-
-	echo  0 > /sys/class/gpio/gpio451/value
-	sleep 10
-	echo  1 > /sys/class/gpio/gpio451/value
 	sleep 1
 
-	echo "Power on"
-	busctl set-property xyz.openbmc_project.State.Host \
-		/xyz/openbmc_project/state/host0 \
-		xyz.openbmc_project.State.Host \
-		RequestedHostTransition s xyz.openbmc_project.State.Host.Transition.On
+    # Enable ME
+    setEnableME false
+    sleep 12
 
-	i=0
-	r=$(cat /sys/class/gpio/gpio506/value)
-	until [ "$r" == "1" -o "$i" == "60" ]
-	do
-		i=$(($i+1))
-		sleep 1
-		r=$(cat /sys/class/gpio/gpio506/value)
-		echo $i"s" " 506:" $r
-	done
+	#echo  0 > /sys/class/gpio/gpio451/value
+	#sleep 10
+	#echo  1 > /sys/class/gpio/gpio451/value
+	#sleep 1
+
+    #if [ "$hostState" == "1" ];then
+    retry=0
+    echo "Power on"
+    PowerOn
+    if [ "$r" == "1" ];then
+        echo "Power on success"
+    else
+        echo "Power on failed..."
+        # i2c may fail, retry boot
+        setEnableME false
+        sleep 12
+        retry=1
+        PowerOn
+    fi
+    #fi
 
 else
 	echo "We could NOT power down HOST, so skip updating BIOS!!"
