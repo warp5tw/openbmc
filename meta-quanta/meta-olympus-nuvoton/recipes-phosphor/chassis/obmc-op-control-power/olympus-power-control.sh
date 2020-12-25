9842309603128085e400d7eb2e1e3a1ba13a60ca
Joseph Liu <kwliu@nuvoton.com>
2020-12-25 11:33:21 +0800

    meta-quanta: olympus-nuvoton: chassis: improve power control
    1. Support force shutdown if power off timeout
    2. Check the pgood pin state before the power operation
    
    Change-Id: I1a7ec0f0a150571dd86a8ef59c930a3626662865
    Signed-off-by: Joseph Liu <kwliu@nuvoton.com>

--- meta-quanta/meta-olympus-nuvoton/recipes-phosphor/chassis/obmc-op-control-power/olympus-power-control.sh
+++ meta-quanta/meta-olympus-nuvoton/recipes-phosphor/chassis/obmc-op-control-power/olympus-power-control.sh
@@ -1,7 +1,11 @@
 echo "set power state $1"
 
-powerTimeOut=90
+powerOnRetry=3
+powerOffRetry=3
+powerOnTimeOut=30
+powerOffTimeOut=90
 powerPluseTime=0.2
+ForcePowerPluseTime=15
 state=$1
 pwrpin=$3
 pgpin=$2
@@ -16,55 +20,68 @@ powerPluse() {
     echo "gpio${pwrpin} output hi"
 }
 
+ForcePowerOff() {
+    echo "Try to ForcePowerOff"
+
+    echo 0 > /sys/class/gpio/gpio${pwrpin}/value
+    echo "gpio${pwrpin} output low"
+    sleep ${ForcePowerPluseTime}
+
+    echo 1 > /sys/class/gpio/gpio${pwrpin}/value
+    echo "gpio${pwrpin} output hi"
+}
+
 PowerOffTimerStart() {
     i=0
     pgood=1
-    until [ "$pgood" == "0" -o "$i" == "${powerTimeOut}" ]
+    until [ "${pgood}" == "0" -o "$i" == "${powerOffTimeOut}" ]
     do
        i=$(($i+1))
        pgood=$(cat /sys/class/gpio/gpio${pgpin}/value)
        sleep 1
     done
 
-    echo $pgood
+    echo ${pgood}
 }
 
 PowerOnTimerStart() {
     i=0
     pgood=0
-    until [ "$pgood" == "1" -o "$i" == "${powerTimeOut}" ]
+    until [ "${pgood}" == "1" -o "$i" == "${powerOnTimeOut}" ]
     do
        i=$(($i+1))
        pgood=$(cat /sys/class/gpio/gpio${pgpin}/value)
        sleep 1
     done
 
-    echo $pgood
+    echo ${pgood}
 }
 
+try=0
+PGOOD=$(cat /sys/class/gpio/gpio${pgpin}/value)
 
 if [ ${state} = "1" ]; then
-  try=0
-  PGOOD=0
-  echo "Power On Start"
-  until [ "$PGOOD" == "1" -o "$try" == "5" ]
-  do
-    try=$(($try+1))
-    powerPluse
-    PGOOD=$(PowerOnTimerStart)
-  done
-  echo "Power On done; pgood=${PGOOD}; Retry=${try}"
+    echo "Power On Start"
+    until [ "${PGOOD}" == "1" -o "${try}" == "${powerOnRetry}" ]
+    do
+      try=$((${try}+1))
+      powerPluse
+      PGOOD=$(PowerOnTimerStart)
+    done
+    echo "Power On done; pgood=${PGOOD}; Retry=${try}"
 else
-  try=0
-  PGOOD=1
-  echo "Power Off Start"
-  until [ "$PGOOD" == "0" -o "$try" == "5" ]
-  do
-    try=$(($try+1))
-    powerPluse
-    PGOOD=$(PowerOffTimerStart)
-  done
-  echo "Power Off done; pgood=${PGOOD}; Retry=${try}"
+    echo "Power Off Start"
+    until [ "${PGOOD}" == "0" -o "${try}" == "${powerOffRetry}" ]
+    do
+      try=$((${try}+1))
+      powerPluse
+      PGOOD=$(PowerOffTimerStart)
+    done
+    echo "Power Off done; pgood=${PGOOD}; Retry=${try}"
+
+    if [ "${PGOOD}" == "1" ]; then
+        ForcePowerOff
+    fi
 fi
 
 if [ $1 = "1" ]; then
